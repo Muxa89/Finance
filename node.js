e3b5b28ed29036bb3caa2110149e04b165f93eba
Mikhail <mikhail@mikhail-k15.10>
2016-04-19 19:57:18 +0400

    Added node.js server config and bower config.

---
+++ node.js
@@ -0,0 +1,6 @@
+const http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(".")).listen(8080, function(){
+	    console.log('Server running on 8080...');
+});
